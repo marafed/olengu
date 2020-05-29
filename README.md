77880f34601bec260510cb574954fe768956bdcf
Federica Marasà <marafed@parrotsec.org>
2020-05-29 16:42:27 +0200

    - finita homepage - creazione pagina di presentazione become_host

--- README.md
+++
@@ -1,5 +0,0 @@
-# olengu
-olengu project for damn university
-
-siamo dei cazzoni che non sanno fare niente ciao
-
---
+++ homepage/main.js
@@ -0,0 +1,12 @@
+function showPopup(id) {
+    document.getElementById(id).style.display = "block";
+}
+function hidePopup(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+
+
+
+
+
