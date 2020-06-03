2ca6c49c653240b5301373b388ab1fab625922d1
Federica Marasà <marafed@parrotsec.org>
2020-06-03 17:37:59 +0200

    salva modifiche

--- homepage/main.js
+++ homepage/main.js
@@ -5,6 +5,22 @@ function hidePopup(id) {
     document.getElementById(id).style.display = "none";
 }
 
+function validateLoginForm() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  }
+
 
 
 
