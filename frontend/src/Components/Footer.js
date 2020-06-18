f1b7cb45d97a6c95dde64afdddfb3ceef92d6edc
Federica Marasà <marafed@parrotsec.org>
2020-06-18 06:35:21 +0200

    include back react frontend

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <div id="footer" className="row">
+      <div className="col">
+        <h4>Informazioni</h4>
+        <p> Chi siamo</p>
+        <p> La nostra piattaforma</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
