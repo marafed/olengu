74186a2b20b9c5e15436494493d49d3e18e3b1b1
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-06-26 18:45:21 +0200

    add banckend db interaction

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var hash = crypto.createHash('sha512');
+
+router.use(express.json())
 
 /* GET users listing. */
 router.post('/register', function(req, res, next) {
   res.send('auth');
 });
 
-router.post('/login', function(req, res, next) {
-  res.send('auth');
-});
+router.post('/login', [
+    check('email').isEmail().normalizeEmail().trim().escape().stripLow(),
+    check('password').isLength({min: 5}).trim().escape().stripLow()
+  ],
+  function(req, res, next) {
+    var email = res.body["email"];
+    var pass = res.body["password"];
+
+    var hashedpass = hash.update(pass, 'utf-8').digest('hex');
+
+    queryres = db.query("select id from user where email = '" + email + "' and password = '" + hashedpass + "';");
+    
+    console.log(queryres);
+    res.send(queryres);
+  }
+);
 
 router.post('/insertitem', function(req, res, next) {
   console.log(req.body);
