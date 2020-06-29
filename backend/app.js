bb43d37defc96f1d2cd1e5c62e6dc8054c69ea19
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-06-29 11:47:05 +0200

    update backend and docker scripts

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require("fs");
-
-
-db = new mysql.createConnection({
-  host: "db",
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-})
-
-dbcon = db.connect( function(err) {
-    if (err) throw err;
-    console.log("database connected")
-    let init_user = "create table if not exists "
-                +"user (id int not null auto_increment, "
-                +"nome varchar(20) not null, "
-                +"cognome varchar(20) not null, "
-                +"email varchar(30) not null, "
-                +"birthday date not null, "
-                +"password varchar(512) not null, "
-                +"primary key (id)"
-                +");";
-    let init_vattelappesca = "create table if not exists "
-                +"miatabella (id int not null auto_increment, "
-                +"nome varchar(20) not null, "
-                +");";
-    db.query(init_user);
-    //db.query(init_vattelappesca);
-    console.log("database initialized");
-});
-
 
 
 var authRouter = require('./routes/auth');
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,29 +2,32 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var hash = crypto.createHash('sha512');
+var db = require('../db');
+const { check, validationResult } = require('express-validator');
+
 
 router.use(express.json())
 
 /* GET users listing. */
-router.post('/register', function(req, res, next) {
+router.post('/register', function(req, res) {
   res.send('auth');
 });
 
 router.post('/login', [
     check('email').isEmail().normalizeEmail().trim().escape().stripLow(),
-    check('password').isLength({min: 5}).trim().escape().stripLow()
-  ],
-  function(req, res, next) {
-    var email = res.body["email"];
-    var pass = res.body["password"];
+    check('password').isLength({min: 5}).trim().escape().stripLow(),
+    function(req, res) {
+      var email = req.body["email"];
+      var pass = req.body["password"];
 
-    var hashedpass = hash.update(pass, 'utf-8').digest('hex');
+      var hashedpass = hash.update(pass, 'utf-8').digest('hex');
 
-    queryres = db.query("select id from user where email = '" + email + "' and password = '" + hashedpass + "';");
-    
-    console.log(queryres);
-    res.send(queryres);
-  }
+      queryres = db.query("select id from user where email = '" + email + "' and password = '" + hashedpass + "';");
+      
+      console.log(queryres);
+      res.send(queryres);
+    }
+  ]
 );
 
 router.post('/insertitem', function(req, res, next) {
