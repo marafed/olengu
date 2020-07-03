911d4410ad862d8b2050ee1fcf4817083aa128b5
sblaker <antolavoro3@gmail.com>
2020-07-03 17:49:05 +0200

    Ho fatto register. Login funziona ma devo aggiungere qualcosa

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,35 +4,85 @@ var crypto = require('crypto');
 var hash = crypto.createHash('sha512');
 var db = require('../db');
 const { check } = require('express-validator');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+const { send } = require('process');
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
+var app = express();
 
-/* GET users listing. */
-router.post('/register', function(req, res) {
-  res.send(req.params);
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+router.post('/login', function(request, response, next) {
+  var email = request.body.email;
+  var password = request.body.password;
+  console.log("---"+email +"-------"+password);
+  let hash = bcrypt.hashSync(password, 10);
+
+  if(bcrypt.compareSync(password, hash)) {
+		console.log("match" + password +"-----"+hash)
+	} else {
+		console.log("Doesn't match")
+		}
+	   
+
+  var sql = "SELECT * FROM user WHERE email = \""+email+"\" and password = \""+hash+"\" ;";
+
+	db.query(sql, function(error, results, fields){
+		if(error){
+			throw error;
+			}
+		else {
+			response.send(JSON.stringify(results)); 
+		}
+	  });
+	  
+	});
+	
+router.post('/register',function(req,res,next){
+	var nome =req.body.name;
+	var cognome = req.body.surname;
+	var bithday = req.body.birthday;
+	var email = req.body.email;
+	var password = req.body.password;
+	
+	console.log(nome +"-"+  cognome +"-"+  bithday +"-"+  email  +"-"+ password);
+	//cripto la password //
+	let hash = bcrypt.hashSync(password, 10);
+	console.log("password cryptata _____" + hash);
+
+	
+	var sql = "INSERT INTO user (nome,cognome,email,birthday,password) VALUES (\""+nome+"\", \""+cognome+"\", \""+email+"\", \""+bithday+"\", \""+hash+"\" );" ;
+	db.query(sql,function(err,results){
+		if (err) throw err;
+      console.log("Utente inserito");
+	});
+	//giusto per vedere quello che mi dava
+	var sql = "SELECT * FROM user WHERE email = \""+email+"\" and password = \""+hash+"\" ;";
+
+	db.query(sql, function(error, results, fields){
+		if(error){
+			throw error;
+			}
+		else {
+			JSON.stringify(results);
+			res.send(JSON.stringify(results)); 
+		}
+	  });
+
+
+	
 });
 
-router.get('/register', function(req, res) {
-  res.send(req.params);
-});
-
-router.post('/login',
-    function(req, res) {
-      var email = ""+req.body.email;
-      var pass = ""+req.body.password;
-      console.log(""+email+" - "+pass);
-      console.log(req.body);
-
-      //var hashedpass = hash.update(pass, 'utf-8').digest('hex');
-
-      db.query("select * from user;", (err, res) => {
-        console.log(res);
-      });
-      res.send("oof");
-    }
-);
 
 router.post('/insertitem', function(req, res, next) {
   console.log(req.body);
