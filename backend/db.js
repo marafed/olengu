8ffa4bdf61733743f8d4ae96ef796680fa5ed0a4
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-07-06 18:35:14 +0200

    update database tables

--- backend/db.js
+++ backend/db.js
@@ -10,82 +10,14 @@ var connection = new mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("database connected")
-    let init_user = "create table if not exists user ("
-        +"id_usr int(32) not null auto_increment,"
-        +"is_host bool,"
-        +"nome varchar(16) not null,"
-        +"cognome varchar(16) not null,"
-        +"data_nascita date,"
-        +"email varchar(30) not null,"
-        +"pswd varchar(512) not null,"
-        +"guadagno_host int(32),"
-        +"primary key (id_usr)"
-        +");";
-    let init_filtro = "create table if not exists filtro ("
-        +"id_filtri int(32) not null,"
-        +"is_bnb bool not null,"
-        +"n_ospiti int(32) not null,"
-        +"prezzo_notte int(32) not null, "
-        +"n_letti_singoli int(10),"
-        +"n_letti_matr int(10),"
-        +"n_divano_letto int(10), "
-        +"n_camere int(20) not null,"
-        +"n_bagni int(10) not null, "
-        +"colazione bool,"
-        +"AC bool,"
-        +"parcheggio bool,"
-        +"wifi bool,"
-        +"lavatrice bool,"
-        +"baby_friendly bool,"
-        +"kit_cortesia bool,"
-        +"tv bool,"
-        +"asciugacapelli bool,"
-        +"zona_lavoro bool,"
-        +"animali_domestici_ammessi bool,"
-        +"fumo bool,"
-        +"piscina bool,"
-        +"idromassaggio bool,"
-        +"sauna bool,"
-        +"spa bool,"
-        +"palestra bool,"
-        +"immerso_natura bool,"
-        +"vista_mare bool,"
-        +"primary key(id_filtri)"
-    +");"
-    let init_luogo = "create table if not exists luogo ("
-        +"id_luogo int(32) not null,"
-        +"nazione varchar(30) not null,"
-        +"regione varchar(30) not null,"
-        +"provincia varchar(30) not null,"
-        +"paese varchar(30) not null,"
-        +"via varchar(50) not null,"
-        +"numero int(8),"
-        +"info_aggiuntive varchar(300),"
-        +"primary key (id_luogo)"
-    +");";
-    let init_annuncio = "create table if not exists annuncio ("
-        +"id_ann int(32) not null,"
-        +"ref_id_usr int(32) not null,"
-        +"nome_annuncio varchar(150) not null,"
-        +"ref_id_luogo int(32) not null,"
-        +"ref_id_filtri int(32) not null,"
-        +"descrizione varchar(300), "
-        +"attrazioni varchar(300),"
-        +"primary key (id_ann)"
-    +");";
-    let init_prenotazione = "create table if not exists prenotazione ("
-        +"ref_id_usr int(32) not null,"
-        +"ref_id_ann int(32) not null, "
-        +"check_in date not null, "
-        +"check_out date not null, "
-        +"primary key(ref_id_usr, ref_id_ann, check_in)"
-    +");"
+    console.log("database connected");
+    
+    let init_user = "create table user ( id_usr int(32) not null auto_increment, is_host bool default null, nome varchar(16) default null, cognome varchar(16) default null, data_nascita date default null, email varchar(30) default null, pswd varchar(512) default null, guadagno_host int(32) default null, primary key (id_usr));";
+    let init_annunci = "create table annunci ( id_ann int(32) not null auto_increment, ref_id_usr int(32) default null, nome_annuncio varchar(150) default null, luogo varchar(300) default null, via varchar(100) default null, descrizione varchar(300) default null, attrazioni varchar(300) default null, indirizzo varchar(300) default null, is_bnb bool default null, n_ospiti int(32) default null, prezzo_notte int(32) default null, n_letti_singoli int(10) default null, n_letti_matr int(10) default null, n_divano_letto int(10) default null, n_camere int(20) default null, n_bagni int(10) default null, colazione bool default null, AC bool default null, parcheggio bool default null, wifi bool default null, lavatrice bool default null, baby_friendly bool default null, primary key (id_ann));";
+    let init_prenotazioni = "create table prenotazioni ( id_prenotazione int(32) not null auto_increment, ref_id_usr int(32) default null, ref_id_ann int(32) default null, check_in_giorno int(32) default null, check_in_mese int(32) default null, check_in_anno int(32) default null, check_out_giorno int(32) default null, check_out_mese int(32) default null, check_out_anno int(32) default null, stato varchar(255) default null, tot_pagato int(255) default null, primary key(id_prenotazione));";
     db.query(init_user);
-    db.query(init_filtro);
-    db.query(init_luogo);
-    db.query(init_annuncio);
-    db.query(init_prenotazione);
+    db.query(init_annunci);
+    db.query(init_prenotazioni);
     
 
     console.log("database initialized");
