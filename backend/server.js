0366a91396d6924ff4f4b79e26e2f65fcf9d12a9
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-07-06 18:50:06 +0200

    add missing function

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,23 @@ const bodyParser = require('body-parser');
 
 app = express();
 
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+  
+
 var port = normalizePort(process.env.PORT || '3000');
 app.listen(port);
 console.log('server started on port ' + port);
