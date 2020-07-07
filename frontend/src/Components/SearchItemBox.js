40c0c6538f5be6a68f702f623e724ab5099e3e2c
Federica Marasà <marafed@parrotsec.org>
2020-07-08 00:03:57 +0200

    fixcose

--- frontend/src/Components/SearchItemBox.js
+++ frontend/src/Components/SearchItemBox.js
@@ -5,13 +5,8 @@ function SearchItemBox({ item }) {
         <div className="containerFluid">
             <div className="row" id="present-item-box">
                 <div className="col-md-3 col-sm-12 present-item-col">
-<<<<<<< HEAD
-                    <img className="present-item-img" src={item.image.url} />
-                </div> 
-=======
                     {/*<img className="present-item-img" src={item.image.url} />*/}
                 </div>
->>>>>>> 56cff02d3d7bffaac3b0a63d97e2cc0121de57a9
                 <div className="col-md-9 col-sm-12">
                     <h2>{item.nome_annuncio}</h2>
                     <h4>{item.luogo}</h4>
